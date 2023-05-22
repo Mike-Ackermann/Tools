60ae68d152ff7befad839a7704b866895ce11b2d
Mike Ackermann <amike98@vt.edu>
2023-05-22 17:09:22 -0400

    changed stuff

--- PlottingTools/FormatPlot.m
+++ PlottingTools/FormatPlot.m
@@ -1,6 +1,12 @@
 ax = gca;
-Default_TW = ax.TickLength;
-Default_LW = ax.LineWidth;
-ax.TickLength = Default_TW * 2;
-ax.LineWidth = Default_LW * 2;
+% make all lines thicker
+lines = findobj(gcf,'Type','Line');
+for i = 1:numel(lines)
+  lines(i).LineWidth = 2.0;
+end
+% Make all ticks and lines thicker
+ax.TickLength = [.02,.05];
+ax.LineWidth = 1;
+% Make font size larger
 ax.FontSize = 16;
+%ax.TickLabelInterpreter = 'latex';
