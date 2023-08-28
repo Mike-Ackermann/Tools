a845e6a0241cbf2631c4c7ac5b5d6ffe05010316
Mike Ackermann <amike98@vt.edu>
2023-08-28 16:30:35 -0400

    now uses Sylvester equations

--- ModelReductionTools/ROM_algorithms/Loewner.m
+++ ModelReductionTools/ROM_algorithms/Loewner.m
@@ -11,97 +11,7 @@ Hs1 = Hs1(idx1);
 [~,idx2] = sort(imag(s2),'ComparisonMethod','real');
 s2 = s2(idx2);
 Hs2 = Hs2(idx2);
-
 r = length(s1);
-
-Mi = zeros(r); Li = zeros(r);
-B = reshape(Hs1,r,1);
-C = reshape(Hs2,1,r);
-
-for i = 1:r
-    for j = 1:r
-        Li(i,j) = (Hs1(i)-Hs2(j))/(s1(i)-s2(j));
-        Mi(i,j) = (s1(i)*Hs1(i)-s2(j)*Hs2(j))/(s1(i)-s2(j));
-    end
-end
-
-E = -Li;
-A = -Mi;
-
-%% Keep real
-%%%%%%%%%%%%%%%%%%%%%%%
-%check for repeated shifts to not multiply by transformation matrix
-% repeated_shift = NaN(r,1);
-% count = 1;
-% for i = 1:2:r
-%    if s(i) == s(i+1)
-%        repeated_shift(count) = 1;
-%    else
-%        repeated_shift(count) = 0;
-%    end
-%    count = count + 1;
-% end
-% idx = 1:r/2;
-% idx = idx(logical(repeated_shift));
-
-% % repeated_shift = imag(s) == 0;
-% % repeated_idx = zeros(ceil(r/2));
-% % isolated_idx = zeros(ceil(r/2));
-% % for i = 1:2:r
-% %     if repeated_shift(i) && repeated_shift(i+1)
-% %         repeated_idx(floor(i/2)) = 1;
-% %     elseif repeated_shift(i)
-% %         isolated_idx(floor(i/2)) = 1;
-% %     end
-% % end
-
-%put the real values in s first
-% real_idx = imag(s) == 0;
-% indicies = 1:r;
-% real_idx = indicies(real_idx);
-% s_imag = s(~real_idx);
-% s_real = s(real_idx);
-% I = eye(2);
-% T1 = [1 1; 1i -1i];
-% T1c = repmat({T1},1,r/2);
-% 
-% T1inv = (1/2)*[1 -1i; 1 1i];
-% count = 1;
-% T = [];
-% Tinv = [];
-% while count <= r
-%     if ismember(count,real_idx)
-%         T =blkdiag(T,1);
-%         Tinv = blkdiag(Tinv,1);
-%         count = count + 1;
-%     else
-%         T = blkdiag(T,T1);
-%         Tinv = blkdiag(Tinv,T1inv);
-%         count = count + 2;
-%     end
-% end
-% T1invc = repmat({T1inv},1,r/2);
-% for i = 1:length(idx)
-%    T1c{idx(i)} = I;
-%    T1invc{idx(i)} = I;
-% end
-% 
-% Tinv = blkdiag(T1c{:});
-% T = blkdiag(T1invc{:});
-% 
-% B = Tinv*Bi;
-% C = Ci*T;
-% A = Tinv*Ai*T;
-% E = Tinv*Ei*T;
-% L = Tinv*Li*T;
-% M = Tinv*Mi*T;
-% 
-% E = real(E);
-% A = real(A);
-% B = real(B);
-% C = real(C);
-% L = real(L);
-% M = real(M);
 
 % Solve via Sylvester equations
 Z_R = [];
@@ -150,11 +60,11 @@ M_R = M_R.';
 
 
 %%%%%%%%%%%%%%%%%%%%%
-Bp = reshape(Z_R,r,1);
-Cp = reshape(Y_R,1,r);
-Ep = -L_R;
-Ap = -M_R;
-return
+B = reshape(Z_R,r,1);
+C = reshape(Y_R,1,r);
+E = -L_R;
+A = -M_R;
+
 
 
 %%%%%%%%%%%%%%%%%%%%%
